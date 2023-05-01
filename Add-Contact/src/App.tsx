c25930c04b13a0675bf80cb0072a71544a850af9
ehsanm <ehsanmohamadi780@gmail.com>
2023-05-01 19:54:13 +0430

    add contact list component

--- Add-Contact/src/App.tsx
+++ Add-Contact/src/App.tsx
@@ -1,10 +1,12 @@
+import ContactList from './layout/ContactList/ContactList';
+import './index.css';
+
 function App() {
-
-
   return (
     <>
+      <ContactList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
